8084a6f3be2684fc24f99e0d8a2faf6b5f65ccbb
Renz <@whilcerenzmacaambac@gmail.com>
2022-11-28 09:45:07 +0800

    added timestamps in migration

--- database/migrations/2022_11_16_083814_create_inbox_table.php
+++ database/migrations/2022_11_16_083814_create_inbox_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('phone_number');
             $table->string('message');
             $table->integer('user_id');
+            $table->timestamps();
         });
     }
 
